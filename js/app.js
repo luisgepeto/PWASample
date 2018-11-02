f4ff7073dc27a42b60740f91197739ef39ba4278
luisgepeto <luisgepeto@gmail.com>
2018-11-02 17:03:36 -0600

    first commit

---
+++ js/app.js
@@ -0,0 +1,11 @@
+var carService = require('./carService.js');
+
+window.pageEvents= {
+    loadCarPage: function(carId){
+        carService.loadCarPage(carId);
+    },
+    loadMore: function(){
+        carService.loadMoreRequest();
+    }
+}
+carService.loadMoreRequest();
